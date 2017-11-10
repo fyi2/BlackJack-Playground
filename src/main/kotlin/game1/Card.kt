18539e542560b80e6f89c5cbe0fd41c35165a932
fyi2 <tony@sherman.org>
2017-11-10 12:24:36 -0500

    Added Suit Getters

--- src/main/kotlin/game1/Card.kt
+++ src/main/kotlin/game1/Card.kt
@@ -1,4 +1,17 @@
 package game1
 
 class Card(var value:Int, var suit:Int) {
+
+    val suitName:String get(){
+        when(suit){
+            1-> return "Spades"
+            2->return "Hearts"
+            3->return "Clubs"
+            4->return "Diamonds"
+            else-> return throw IllegalStateException()
+        }
+    }
+    val valueName:String get(){
+        return "Ace"
+    }
 }
