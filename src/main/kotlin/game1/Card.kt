944020845de1f7be2a69d58c26554e97bfb93eee
fyi2 <tony@sherman.org>
2017-11-10 12:40:10 -0500

    Added value and points Getters

--- src/main/kotlin/game1/Card.kt
+++ src/main/kotlin/game1/Card.kt
@@ -1,17 +1,31 @@
 package game1
 
-class Card(var value:Int, var suit:Int) {
+class Card(var value: Int, var suit: Int) {
 
-    val suitName:String get(){
-        when(suit){
-            1-> return "Spades"
-            2->return "Hearts"
-            3->return "Clubs"
-            4->return "Diamonds"
-            else-> return throw IllegalStateException()
+    val suitName: String
+        get() {
+            when (suit) {
+                1 -> return "Spades"
+                2 -> return "Hearts"
+                3 -> return "Clubs"
+                4 -> return "Diamonds"
+                else -> return throw IllegalStateException()
+            }
         }
-    }
-    val valueName:String get(){
-        return "Ace"
+    val valueName: String
+        get() = when (value) {
+            1 -> "Ace"
+            in 2..10 -> value.toString()
+            11 -> "Jack"
+            12 -> "Queen"
+            13 -> "King"
+            else -> "Joker"
+        }
+    val name:String get() = "$valueName of $suitName"
+
+    val points:Int get() = when(value){
+        in 1..9-> value
+        in 10..13 -> 10
+        else -> 0
     }
 }
